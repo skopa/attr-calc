12a33cedf1e7a6dc8c1484abc10c76c78113ccda
Stepan Skopivskiy <stepan@skop.in.ua>
2019-12-03 10:38:54 +0200

    Changed width of table

--- resources/js/components/Attributes/AttributesListComponent.vue
+++ resources/js/components/Attributes/AttributesListComponent.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-10">
             <div class="card card-default">
                 <div class="card-header">
                     <b>Attributes</b>
